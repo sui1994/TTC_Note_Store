ec1a68003d500cf9e35ba9c525e539d0410c1ba5
sui1994 <130881956+sui1994@users.noreply.github.com>
2025-05-21 18:05:43 +0900

    feature: ヘッダーコンポーネントを追加し、レイアウトに組み込む

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP} from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body
         className={notoSansJP.className + " bg-background text-foreground"}
       >
+      <Header />
         {children}
       </body>
     </html>
