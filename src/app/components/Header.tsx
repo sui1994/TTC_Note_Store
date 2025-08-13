2bda8597318e3a1a0bb5699ea6bd570beff0f7d1
sui1994 <130881956+sui1994@users.noreply.github.com>
2025-08-13 09:48:56 +0900

    fix(hydration): Headerの初回描画差異を防ぐ修正

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -27,9 +27,14 @@ const Header = () => {
               </Link>
             </>
           ) : (
+            <>
             <Link href="/login" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
               ログイン
             </Link>
+            <Link href="/login">
+              <Image width={50} height={50} alt="default_icon" src="/default_icon.png" className="rounded-full border-2 border-gray-300 hover:border-white transition-colors" />
+            </Link>
+          </>
           )}
         </div>
       </nav>
