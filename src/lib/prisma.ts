e53125c2e04354b7b48de0f8a09f645e14a4757f
sui1994 <130881956+sui1994@users.noreply.github.com>
2025-08-17 13:09:37 +0900

    fix/PrismaClientInitializationErrorの対応

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -12,9 +12,4 @@ export const prisma =
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-// Ensure connection is established
-prisma.$connect().catch((error) => {
-  console.error("Failed to connect to database:", error);
-});
-
 export default prisma;
