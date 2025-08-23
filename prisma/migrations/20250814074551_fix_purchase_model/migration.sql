13654b37d27edf60f4283bbce445a6f5affa73d0
sui1994 <130881956+sui1994@users.noreply.github.com>
2025-08-23 14:20:52 +0900

    fix/コメント削除

--- prisma/migrations/20250814074551_fix_purchase_model/migration.sql
+++ prisma/migrations/20250814074551_fix_purchase_model/migration.sql
@@ -1,16 +1,9 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Purchases` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
 ALTER TABLE "public"."Purchases" DROP CONSTRAINT "Purchases_userId_fkey";
 
--- DropTable
+
 DROP TABLE "public"."Purchases";
 
--- CreateTable
+
 CREATE TABLE "public"."Purchase" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
@@ -20,5 +13,5 @@ CREATE TABLE "public"."Purchase" (
     CONSTRAINT "Purchase_pkey" PRIMARY KEY ("id")
 );
 
--- AddForeignKey
+
 ALTER TABLE "public"."Purchase" ADD CONSTRAINT "Purchase_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
