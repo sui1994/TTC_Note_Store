d92260a0f5a07e439879bf873819c0f5ccefe530
sui1994 <130881956+sui1994@users.noreply.github.com>
2025-08-31 14:24:42 +0900

    Add loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,29 @@
+﻿"use client";
+
+import React from "react";
+import { ClipLoader } from "react-spinners";
+
+interface LoadingSpinnerProps {
+  size?: number;
+  color?: string;
+}
+
+const LoadingSpinner = ({ size = 50, color = "#123abc" }: LoadingSpinnerProps) => {
+  return (
+    <div className="spinner-container flex items-center justify-center min-h-screen">
+      <ClipLoader size={size} color={color} />
+
+      {/* スタイル */}
+      <style jsx>{`
+        .spinner-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
